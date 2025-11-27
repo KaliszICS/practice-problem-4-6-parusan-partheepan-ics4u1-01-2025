0cd9df3ac0d8dc0e05c35e750fe0e32411f3d9d8
Parusan <parusan.partheepan@student.tdsb.on.ca>
2025-11-27 18:44:34 -0500

    make new changes

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
 
+/**
+ * Represents a Person with personal information and family relationships.
+ * Stores name, birth date, social security number, and maintains a list of siblings.
+ */
 public class Person {
     private String firstname;
     private String lastname;
@@ -10,7 +14,17 @@ public class Person {
     private int numberOfSiblings = 0;  // Default value 0
     private Person[] siblings = new Person[0];  // Default empty array
 
-    // Constructor - takes all values except numberOfSiblings and siblings
+    /**
+     * Constructs a new Person with the specified personal information.
+     * Siblings array starts empty and can be populated using addSibling().
+     *
+     * @param firstname the person's first name, cannot be null
+     * @param lastname the person's last name, cannot be null
+     * @param birthDay the day of birth (1-31)
+     * @param birthMonth the month of birth (1-12)
+     * @param birthYear the year of birth
+     * @param ssn the social security number, cannot be null
+     */
     public Person(String firstname, String lastname, int birthDay, 
                   int birthMonth, int birthYear, String ssn) {
         this.firstname = firstname;
@@ -21,29 +35,57 @@ public class Person {
         this.ssn = ssn;
     }
 
-    // Getters and setters for firstname and lastname
+    /**
+     * Returns the person's first name.
+     *
+     * @return the first name
+     */
     public String getFirstname() {
         return firstname;
     }
 
+    /**
+     * Sets the person's first name.
+     *
+     * @param firstname the new first name, cannot be null
+     */
     public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
 
+    /**
+     * Returns the person's last name.
+     *
+     * @return the last name
+     */
     public String getLastname() {
         return lastname;
     }
 
+    /**
+     * Sets the person's last name.
+     *
+     * @param lastname the new last name, cannot be null
+     */
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-    // Getter for birthday in "day/month/year" format
+    /**
+     * Returns the person's birthday in "day/month/year" format.
+     *
+     * @return the formatted birthday string
+     */
     public String getBirthday() {
         return this.birthDay + "/" + this.birthMonth + "/" + this.birthYear;
     }
 
-    // Add a sibling to the siblings array
+    /**
+     * Adds a sibling to this person's list of siblings.
+     * Increases the sibling count and expands the siblings array.
+     *
+     * @param sibling the sibling to add, cannot be null
+     */
     public void addSibling(Person sibling) {
         // Create a new array that's one size larger
         Person[] newSiblings = Arrays.copyOf(siblings, siblings.length + 1);
@@ -58,28 +100,57 @@ public class Person {
         this.numberOfSiblings++;
     }
 
-    // Getter for siblings array
+    /**
+     * Returns the array of siblings for this person.
+     * Returns an empty array if no siblings have been added.
+     *
+     * @return the siblings array (may be empty)
+     */
     public Person[] getSiblings() {
         return siblings;
     }
 
-    // Additional getters for other fields (not required but useful)
+    /**
+     * Returns the day of birth.
+     *
+     * @return the birth day (1-31)
+     */
     public int getBirthDay() {
         return birthDay;
     }
 
+    /**
+     * Returns the month of birth.
+     *
+     * @return the birth month (1-12)
+     */
     public int getBirthMonth() {
         return birthMonth;
     }
 
+    /**
+     * Returns the year of birth.
+     *
+     * @return the birth year
+     */
     public int getBirthYear() {
         return birthYear;
     }
 
+    /**
+     * Returns the social security number.
+     *
+     * @return the SSN
+     */
     public String getSsn() {
         return ssn;
     }
 
+    /**
+     * Returns the number of siblings for this person.
+     *
+     * @return the sibling count
+     */
     public int getNumberOfSiblings() {
         return numberOfSiblings;
     }
